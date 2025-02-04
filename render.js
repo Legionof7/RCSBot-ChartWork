6452d88e53761462fc82963673def6616fe91905
Legionof7 <790161-Legionof7@users.noreply.replit.com>
2025-02-04 00:30:09 +0000

    User checkpoint: Replace sharp with canvas for SVG to PNG conversion.  Update chart data to defaults.

--- render.js
+++ render.js
@@ -1,25 +1,16 @@
 
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import fs from 'fs';
-import sharp from 'sharp';
-import ChartComponent from './VictoryChart.js';
+    import React from 'react';
+    import ReactDOMServer from 'react-dom/server';
+    import fs from 'fs';
+    import { createCanvas } from 'canvas';
+    import ChartComponent from './VictoryChart.js';
 
-const chartData = {"type": "bar", "config": {"data": [{"x": "HDL", "y": 55}], "title": "HDL Cholesterol", "xlabel": "Test", "ylabel": "Value (mg/dL)", "referenceLines": {"HDL": [40, 60]}}};
+    const canvas = createCanvas(800, 600);
+    const ctx = canvas.getContext('2d');
+    const chartData = {"type": "bar", "config": {"data": [], "title": "Graph", "xlabel": "X Axis", "ylabel": "Y Axis", "referenceLines": {}}};
 
-const element = React.createElement(ChartComponent, {graphData: chartData});
-const svg = ReactDOMServer.renderToString(element);
+    const element = React.createElement(ChartComponent, {graphData: chartData});
+    const svg = ReactDOMServer.renderToString(element);
 
-// Wrap SVG in proper XML wrapper
-const fullSvg = `
-<svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">
-  ${svg}
-</svg>
-`;
-
-// Convert SVG to PNG using sharp
-sharp(Buffer.from(fullSvg))
-  .resize(800, 600)
-  .png()
-  .toFile('/tmp/tmpsxxe2_1i/chart.png')
-  .catch(err => console.error('Error converting SVG to PNG:', err));
+    fs.writeFileSync('/tmp/tmpsxxe2_1i/chart.png', canvas.toBuffer());
+    
